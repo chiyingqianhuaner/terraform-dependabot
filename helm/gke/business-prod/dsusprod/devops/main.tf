105ec742e8a9181d8e66087b8d1eca8f57401d10
chiyingqianhuaner <ccai@liveramp.com>
2024-10-23 08:58:27 +0800

    test

--- helm/gke/business-prod/dsusprod/devops/main.tf
+++ helm/gke/business-prod/dsusprod/devops/main.tf
@@ -1,6 +1,13 @@
 module "honeypot_template" {
   source  = "terraform-google-modules/vm/google//modules/instance_template"
-  version = "10.0.0"
+  version = "7.9.0"
 
-  region = "us-central1"
+  region     = "us-central1"
+}
+
+module "cloud-storage_template" {
+  source  = "terraform-google-modules/cloud-storage/google"
+  version = "4.0.0"
+
+  region     = "us-central1"
 }
